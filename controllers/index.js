49a6c76538f36f77bdb5091125b716bd958b5075
La Scalea <rodrigolascalea@tokenlab.com.br>
2020-04-07 13:50:36 -0300

    problema key.includes

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,48 @@
 const models = require('../database/models');
 
-const createUser = async (req, res) => {
+const criarUsuario = async (req, res) => {
   console.dir(req.body);
   try {
-    const user = await models.User.create(req.body);
-    return res.status(201).json({ user, });
+    const usuario = await models.Usuario.create(req.body);
+    return res.status(201).json({ usuario });
+  } catch (error) {
+    return res.status(500).json({error: error.message})
+  }
+}
+
+const listaUsuarios = async (req, res) => {
+  try {
+    const usuarios = await models.Usuario.findAll();
+    return res.status(200).json({ usuarios });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+const criarTelefone = async (req, res) => {
+  console.dir(req.body);
+  console.dir("---------||---------");
+  console.dir(req.params);
+  try {
+    const { USUA_NR_IDENTIFICADOR } = req.params;
+    const { TELE_NR_DDD, TELE_TX_NUMERO } = req.body
+
+    const usuario = await models.Usuario.findByPk(USUA_NR_IDENTIFICADOR);
+
+    if(!usuario){
+      return res.status(400).json({error: 'Usuário não encontrado'})
+    }
+    else{
+      const telefone = await models.Telefone.create(TELE_NR_DDD, TELE_TX_NUMERO, USUA_NR_IDENTIFICADOR);
+      return res.status(201).json({ telefone });
+    }
   } catch (error) {
     return res.status(500).json({error: error.message})
   }
 }
 
 module.exports = {
-  createUser,
+  criarUsuario,
+  listaUsuarios,
+  criarTelefone
 }
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const router = Router();
 
 router.get('/', (req, res) => res.send('Welcome'))
 
-router.post('/user', controllers.createUser);
+router.get('/usuarios', controllers.listaUsuarios);
+router.post('/usuarios', controllers.criarUsuario);
+
+router.post('/usuarios/:USUA_NR_IDENTIFICADOR/telefones', controllers.criarTelefone);
 
 module.exports = router;
